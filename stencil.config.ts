d8ebbb24695a79c98e842fe0e36e0be5d33ef633
Andre Gagnon <ajgagnon@uwalumni.com>
2019-04-10 14:52:46 -0500

    Update readme.

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,10 +3,10 @@
  * https://stenciljs.com/docs/config
  */
 // Needed for type issue with TS for protocol option in devServer
-import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
-type Protocol = 'http' | 'https';
+type Protocol = "http" | "https";
 type Port = number;
 type Address = string;
 type Name = string;
@@ -20,11 +20,11 @@ interface IstencilPkg {
 }
 
 // Constants for the file
-const DEFAULT_NAME = 'my-component';
+const DEFAULT_NAME = "project-huddle-ui";
 const DEFAULT_PORT = 3333;
-const DEFAULT_HOST = 'localhost';
-const DEFAULT_PROTOCOL = 'http';
-const DEFAULT_BUILD_DIR = 'build';
+const DEFAULT_HOST = "localhost";
+const DEFAULT_PROTOCOL = "http";
+const DEFAULT_BUILD_DIR = "build";
 
 /**
  * Function to get the resouces URL for build and prod environments
@@ -39,18 +39,26 @@ export const getResourcesURL = (
   rH: Address = DEFAULT_HOST,
   rT: Port = DEFAULT_PORT,
   rF: Name = DEFAULT_NAME,
-  rB: BuildDir = DEFAULT_BUILD_DIR,
-) => (process.env.STENCIL_ENV === 'dev' ? `${rP}://${rH}:${rT}/${rB}/${rF}/` : `/${rB}/${rF}/`);
+  rB: BuildDir = DEFAULT_BUILD_DIR
+) =>
+  process.env.STENCIL_ENV === "dev"
+    ? `${rP}://${rH}:${rT}/${rB}/${rF}/`
+    : `/${rB}/${rF}/`;
 
 /**
  * Import the name and stencil keys from package.json
  * name: Will be used as the namespace for the stencil config.
  * stencil: Will be used to get the details of how the dev server should be deployed.
  */
-import { name, stencil } from './package.json';
+import { name, stencil } from "./package.json";
 
 // Assign defaults when importing from package.json
-const { host = 'localhost', port = 3333, protocol = 'http', buildDir = 'build' }: IstencilPkg = stencil as IstencilPkg;
+const {
+  host = "localhost",
+  port = 3333,
+  protocol = "http",
+  buildDir = "build"
+}: IstencilPkg = stencil as IstencilPkg;
 
 /**
  * Export the stencil config to be used with the StencilJS CLI
@@ -64,32 +72,38 @@ export const config: Config = {
     address: host as Address,
     port: port as Port,
     protocol: protocol as Protocol,
-    openBrowser: false,
+    openBrowser: false
   },
   outputTargets: [
-    { type: 'dist' },
-    { type: 'docs' },
+    { type: "dist" },
+    { type: "docs" },
     /**
      * Output target `www` needed for publishing storybook and hosting them.
      */
     {
-      type: 'www',
+      type: "www",
       serviceWorker: null, // disable service workers
       /**
        * The `resourcesUrl` key is needed for stencil to resolve relative paths in dev server mode.
        * Since we are using the `stencil devServer` to serve the output files to `storybooks' devServer`,
        * We need resolve the relative paths on `stencil devServer`.
        */
-      resourcesUrl: getResourcesURL(protocol as Protocol, host, port, name, buildDir),
+      resourcesUrl: getResourcesURL(
+        protocol as Protocol,
+        host,
+        port,
+        name,
+        buildDir
+      ),
       buildDir
-    },
+    }
   ],
-  globalStyle: 'src/globals/variables.css',
+  globalStyle: "src/globals/variables.css",
   plugins: [
     sass({
       injectGlobalPaths: [
-        'src/globals/variables.scss',
-        'src/globals/mixins.scss'
+        "src/globals/variables.scss",
+        "src/globals/mixins.scss"
       ]
     })
   ]
